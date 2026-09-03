455db4829490129f29eb56201b07ffe464222a94
sakethram418 <gsaketh418@gmail.com>
2026-09-03 22:19:44 +0530

    Refactor LCD defines and add new commands

--- lcd_defines.h
+++ lcd_defines.h
@@ -1,33 +1,41 @@
-#define LCD_DEFINES_H
+   //lcd.h
+#include <lpc21xx.h>
+#include"defines.h"
+#include "delay.h"
+#include "types.h"
 
-#define LCD_DATA_MASK       0x000000FF
+//PINS DEFINE
+#define RS 14
+#define EN 15
+#define LCD_PINS 6
 
-#define LCD_RS              (1 << 16)
-#define LCD_EN              (1 << 17)
 
-#define LCD_CLEAR           0x01
-#define LCD_HOME            0x02
+//LCD COMMANDS
+#define CLEAR_LCD         0x01
+#define RET_CUR_HOME      0x02
+#define SHIFT_CUR_RIGHT   0x06
+#define SHIFT_CUR_LEFT    0x07
+#define DSP_OFF            0x08
+#define DSP_ON_CUR_OFF    0x0C
+#define DSP_ON_CUR_ON     0x0E
+#define DSP_ON_CUR_BLK    0x0F
+#define SHIFT_DSP_LEFT    0x10
+#define SHIFT_DSP_RIGHT   0x14
+#define MODE_8BIT_1LINE   0x30
+#define MODE_4BIT_1LINE   0x20
+#define MODE_8BIT_2LINE   0x38
+#define MODE_4BIT_2LINE   0x28
+#define GOTO_LINE1_POS0   0x80
+#define GOTO_LINE2_POS0   0xC0
+#define GOTO_LINE3_POS0   0x94
+#define GOTO_LINE4_POS0   0xD4
+#define GOTO_CGRAM_START  0x40
 
-#define LCD_ENTRY_DEC_SHIFT 0x04
-#define LCD_ENTRY_DEC       0x05
-#define LCD_ENTRY_INC_SHIFT 0x06
-#define LCD_ENTRY_INC       0x07
 
-#define LCD_DISPLAY_OFF     0x08
-#define LCD_DISPLAY_ON      0x0C
-#define LCD_CURSOR_ON       0x0E
-#define LCD_CURSOR_BLINK    0x0F
 
-#define LCD_CURSOR_LEFT     0x10
-#define LCD_CURSOR_RIGHT    0x14
-#define LCD_DISPLAY_LEFT    0x18
-#define LCD_DISPLAY_RIGHT   0x1C
-
-#define LCD_8BIT_1LINE_5X8  0x30
-#define LCD_8BIT_2LINE_5X8  0x38
-#define LCD_8BIT_1LINE_5X10 0x34
-#define LCD_8BIT_2LINE_5X10 0x3C
-
-#define LCD_LINE1           0x80
-#define LCD_LINE2           0xC0
-
+void init_lcd(void);
+void lcd_write_8bit(u8 data);
+void lcd_cmd(u8 cmd);
+void lcd_str(s8 *ptr);
+void lcd_write_int(u32 data);
+void lcd_cgram_write(u8 *p);
