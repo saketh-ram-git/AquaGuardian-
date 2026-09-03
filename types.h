9e118ec7197c625b662147bcacd9e6ebd81dea8b
sakethram418 <gsaketh418@gmail.com>
2026-09-03 22:33:56 +0530

    Add files via upload

---
+++ types.h
@@ -0,0 +1,12 @@
+//types.h
+// typedefines of the the portablity
+typedef unsigned long int u64;
+typedef unsigned int u32;
+typedef unsigned short int  u16;
+typedef unsigned char u8;
+typedef float f32 ;
+typedef double d64;
+typedef signed int s32 ;
+typedef signed short int s1;
+typedef signed long int s64   ;
+typedef char s8 ;
